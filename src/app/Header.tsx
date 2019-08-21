8a505cf19707f305ce4138748c7930abf9dc1da9
Timothy McDonald <tim.mcdonald@deepend.melbourne>
2019-08-21 10:25:38 +1000

    favicon and url change for github repo

--- src/app/Header.tsx
+++ src/app/Header.tsx
@@ -10,7 +10,7 @@ export class Header extends Component {
 					<SocialIcons/>
 					<div className="header--intro">
 						<p className="h3 weight-thin">Hi, I&#39;m Tim McDonald. I am a London based contractor who specialises in <strong>Front End Web Development</strong>, with a background in <strong>design</strong>. I love taking great designs and making them into beautiful websites.</p>
-						<p className="h3 weight-thin">You can see many of my recent projects below, and filter through by either using the search box or clicking on the skillsets listed underneath. I built this page with <strong>react.js</strong>. You can <strong><a href="https://github.com/timmkd/reactportfolio">view the source code on github</a></strong>.</p>
+						<p className="h3 weight-thin">You can see many of my recent projects below, and filter through by either using the search box or clicking on the skillsets listed underneath. I built this page with <strong>react.js</strong>. You can <strong><a href="https://github.com/timmkd/cv">view the source code on github</a></strong>.</p>
 					</div>
 				</div>
 			</header>
